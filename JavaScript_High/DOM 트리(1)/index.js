3fb4fbc80505b0e2516611c035129a746e8e88e1
root <tmdwns950428@naver.com>
2022-06-26 13:59:53 +0900

    자바스크립트 중급!

---
+++ JavaScript_High/DOM 트리(1)/index.js
@@ -0,0 +1,15 @@
+const myTag = document.querySelector("#list-1");
+
+console.log(myTag);
+
+// 형제 요소 노드
+console.log(myTag.previousElementSibling);
+console.log(myTag.nextElementSibling);
+
+// 부모 요소 노드
+console.log(myTag.parentElement);
+
+// 자식 요소 노드
+console.log(myTag.children[1]);
+console.log(myTag.firstElementChild);
+console.log(myTag.lastElementChild);
---
+++ JavaScript_High/요소 노드 프로퍼티/index.js
@@ -0,0 +1,15 @@
+const myTag = document.querySelector("#list-1");
+
+// textContent
+// console.log(myTag.textContent);
+// myTag.textContent = "<h2>title2</h2>"
+
+//outerHTML
+// console.log(myTag.outerHTML);
+// myTag.outerHTML += `<ul>
+//                       <li>Grand Canient</li>
+//                     </ul>`;
+
+//innerHTML
+// console.log(myTag.innerHTML);
+// myTag.innerHTML += `<li>Grand Canient</li>`;
