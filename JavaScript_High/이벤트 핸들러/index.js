902813288e8cbed72c267f0233a25ecf90bf4b74
root <tmdwns950428@naver.com>
2022-06-26 17:46:51 +0900

    자바스크립트 중급!

---
+++ JavaScript_High/이벤트 핸들러/index.js
@@ -0,0 +1,30 @@
+// 이벤트 등록하기
+let btn = document.querySelector('#myBtn');
+
+// btn.onclick = function() {
+//  console.log('Hi Codeit!'); 
+// }
+
+// function event1() {
+//   console.log('Hi Codeit!');
+// }
+
+// function event2() {
+//   console.log('Hi again!');
+// }
+
+// // elem.addEventListener(event, handler)
+// btn.addEventListener('click', event1);
+// btn.addEventListener('click', event2);
+
+// // elem.removeEventListener(event, handler)
+// btn.removeEventListener('click', event2);
+
+// btn.addEventListener('click', function(){
+//   console.log('nyamnyam');
+// });
+
+// // 이렇게 해도 안지워짐
+// btn.removeEventListener('click', function(){
+//   console.log('nyamnyam');
+// });
